6299c19c52c573d46f3c5c1bcfebf45198709183
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-15 07:08:17 +0800

    changed index.js and added styles for index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/app/app';
+
+
+// properties and behavior of components: Theory
+// class WhoAmI extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       years: 18
+//     }
+//     // this.nextYear = this.nextYear.bind(this)
+//   }
+//   nextYear = () => {
+//     this.setState(state => ({
+//       years: ++state.years
+//     }))
+//   }
+//   render() {
+//     const {name, surname, link} = this.props
+//     const {years} = this.state
+//
+//     return (
+//       <>
+//         <button onClick={this.nextYear}>years++</button>
+//         <h1>My name is {name}, surname - {surname}, years - {years}</h1>
+//         <a href={link}>My Profile</a>
+//       </>
+//     )
+//   }
+// }
+
+// const All = () => {
+//   return (
+//       <>
+//         <WhoAmI name="John" surname="Smith" link="#"/>
+//         <WhoAmI name="das" surname="das" link="#"/>
+//         <WhoAmI name="eqw" surname="eqw" link="#"/>
+//       </>
+//   )
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
+    // <All/>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
