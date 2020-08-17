80783050fc18f143b6034facf90a0a08390243d5
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-17 16:22:11 +0800

    added search functional to search-panel

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {Component} from "react";
 import './search-panel.css'
 
-const SearchPanel = () => {
-  return (
-    <input
-        type="text"
-        className="form-control search-input"
-        placeholder="Поиск по записям"
-    />
-  )
+class SearchPanel extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: ''
+    }
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value
+    this.setState({term})
+    this.props.onUpdateSearch(term)
+  }
+
+  render() {
+    return (
+        <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Поиск по записям"
+            onChange={this.onUpdateSearch}
+        />
+    )
+  }
 }
 
 export default SearchPanel
