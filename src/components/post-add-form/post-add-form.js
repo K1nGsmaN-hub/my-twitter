f9abd72433fe7477159e33db41b5c6099b4e513a
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-17 16:23:12 +0800

    added add post functional to post-add-form

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
 import './post-add-form.css'
 
-const PostAddForm = ({onAdd}) => {
-  return (
-      <div className="bottom-panel d-flex" action="">
-        <input
-            type="text"
-            placeholder="О чем вы думаете?"
-            className="form-control new-post-label"
-        />
-        <button type="submit"
-                className="btn btn-outline-secondary"
-                onClick={() => onAdd('Hello world!')}
+class PostAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  onValueChange = (e) => {
+    this.setState({text: e.target.value})
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.onAdd(this.state.text)
+    this.setState({text: ''})
+  }
+
+  render() {
+    return (
+        <form
+            className="bottom-panel d-flex"
+            action=""
+            onSubmit={this.onSubmit}
         >
-          Добавить
-        </button>
-      </div>
-  )
+          <input
+              type="text"
+              placeholder="О чем вы думаете?"
+              className="form-control new-post-label"
+              onChange={this.onValueChange}
+              value={this.state.text}
+          />
+          <button type="submit"
+                  className="btn btn-outline-secondary"
+          >
+            Добавить
+          </button>
+        </form>
+    )
+  }
 }
 
 export default PostAddForm
