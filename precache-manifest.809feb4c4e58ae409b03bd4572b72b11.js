2187ca809c0106783ba51acbd28c2cf16b1f0ba1
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-31 00:19:28 +0800

    Updates

---
+++ precache-manifest.809feb4c4e58ae409b03bd4572b72b11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e35f2b58563fbf9972251636c78de1",
+    "url": "/my-twitter/index.html"
+  },
+  {
+    "revision": "e9e7422d921770f9ce9c",
+    "url": "/my-twitter/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ca11c14e0ab87dc3e241",
+    "url": "/my-twitter/static/css/main.03755e3b.chunk.css"
+  },
+  {
+    "revision": "e9e7422d921770f9ce9c",
+    "url": "/my-twitter/static/js/2.70f44689.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-twitter/static/js/2.70f44689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca11c14e0ab87dc3e241",
+    "url": "/my-twitter/static/js/main.5f8284d4.chunk.js"
+  },
+  {
+    "revision": "2216593d278f07beaf71",
+    "url": "/my-twitter/static/js/runtime-main.632cba0d.js"
+  }
+]);
